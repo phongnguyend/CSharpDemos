3c5ddcf317700846b7af4bf557df7bf3f0fe5348
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-06-10 20:41:46 +0700

    C# Fundamentals

---
+++ CSharpFundamentals/ErrorHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WithoutExceptionHandling();
+            WithExceptionHandling1();
+        }
+
+        private static void WithoutExceptionHandling()
+        {
+            Console.WriteLine("Please enter x:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter y:");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"x/y = {x / (decimal)y}");
+        }
+
+        private static void WithExceptionHandling1()
+        {
+            try
+            {
+                Console.WriteLine("Please enter x:");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter y:");
+                int y = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"x/y = {x / (decimal)y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
