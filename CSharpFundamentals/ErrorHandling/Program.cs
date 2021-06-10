d2fc7f6092943b123387066ce02d39a50d77346e
Phong Nguyen <nguyendoanphong.it@gmail.com>
2021-06-10 22:18:34 +0700

    Error Handling

--- CSharpFundamentals/ErrorHandling/Program.cs
+++ CSharpFundamentals/ErrorHandling/Program.cs
@@ -7,7 +7,19 @@ namespace ErrorHandling
         static void Main(string[] args)
         {
             //WithoutExceptionHandling();
-            WithExceptionHandling1();
+            //WithExceptionHandling1();
+            //WithExceptionHandling2();
+            //WithExceptionHandling3();
+
+            //try
+            //{
+            //    WithExceptionHandling4();
+            //}
+            //catch (MyCustomException ex)
+            //{
+            //    Console.WriteLine("Please retry again.");
+            //    Main(args);
+            //}
         }
 
         private static void WithoutExceptionHandling()
@@ -35,7 +47,110 @@ namespace ErrorHandling
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void WithExceptionHandling2()
+        {
+            try
+            {
+                Console.WriteLine("Please enter x:");
+                int x = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter y:");
+                int y = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"x/y = {x / (decimal)y}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter valid number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Y cannot be 0.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void WithExceptionHandling3()
+        {
+            int x = 0, y = 0;
+            try
+            {
+                Console.WriteLine("Please enter x:");
+                x = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("X must be valid number.");
+            }
+
+            try
+            {
+                Console.WriteLine("Please enter y:");
+                y = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Y must be valid number.");
+            }
+
+            try
+            {
+                Console.WriteLine($"x/y = {x / (decimal)y}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Y cannot be 0.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
+
+        private static void WithExceptionHandling4()
+        {
+            int x = 0, y = 0;
+            try
+            {
+                Console.WriteLine("Please enter x:");
+                x = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("X must be valid number.");
+                throw new MyCustomException("X must be valid number.");
+            }
+
+            try
+            {
+                Console.WriteLine("Please enter y:");
+                y = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Y must be valid number.");
+                throw new MyCustomException("Y must be valid number.");
+            }
+
+            try
+            {
+                Console.WriteLine($"x/y = {x / (decimal)y}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Y cannot be 0.");
+                throw new MyCustomException("Y cannot be 0.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong.");
             }
         }
     }
